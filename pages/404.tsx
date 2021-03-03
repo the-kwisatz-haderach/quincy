ab6eac3ba7e7a01579f83b6bee5e9e930df02796
Gustaf Lundström <gustaflundstrom@Gustafs-MBP.lan>
2021-03-03 14:13:22 +0100

    Add 404 page.

---
+++ pages/404.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React, { ReactElement } from 'react'
+import { useRouter } from 'next/router'
+import { Layout } from '../components/Layout'
+
+interface Props {}
+
+export default function PageNotFound({}: Props): ReactElement {
+  const router = useRouter()
+
+  return (
+    <Layout
+      metaData={{
+        title: '404 - Page Not Found',
+        description: '',
+      }}
+    >
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-8xl my-5">404</h1>
+        <p className="text-xl">Are you lost?</p>
+        <button
+          onClick={() => router.back()}
+          className="mt-10 text-white transition-colors bg-green-600 hover:bg-green-300 rounded p-3"
+        >
+          Go back
+        </button>
+      </div>
+    </Layout>
+  )
+}
