bb6d4a4caeb8a33ccfbfc1fe004973a881769256
Gustaf Lundström <gustaflundstrom@Gustafs-MacBook-Pro.local>
2021-03-05 16:02:41 +0100

    Update postcss config.

--- postcss.config.js
+++ postcss.config.js
@@ -10,18 +10,14 @@ module.exports = {
         'custom-properties': false,
       },
     },
-    '@fullhuman/postcss-purgecss': {
-      content: [
-        './pages/**/*.{js,jsx,ts,tsx}',
-        './components/**/*.{js,jsx,ts,tsx}',
-      ],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      safelist: ['html', 'body'],
-    },
+    // '@fullhuman/postcss-purgecss': {
+    //   content: [
+    //     './pages/**/*.{js,jsx,ts,tsx}',
+    //     './components/**/*.{js,jsx,ts,tsx}',
+    //   ],
+    //   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    //   safelist: ['html', 'body'],
+    // },
     tailwindcss: {},
   },
 }
-
-// module.exports = {
-//   plugins: ['tailwindcss', 'postcss-preset-env'],
-// }
